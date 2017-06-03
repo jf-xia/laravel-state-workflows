4af4446d8ee494f80b5fcaef0f14253d920b538f
Sam <samcharmy@gmail.com>
2017-06-03 21:47:50 +0200

    Add StateWorkflowsServiceProvider and configure autoloading

---
+++ src/Mass6/LaravelStateWorkflows/StateWorkflowsServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mass6\LaravelStateWorkflows;
+
+use Illuminate\Support\ServiceProvider;
+
+class StateWorkflowsServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'../../migrations');
+    }
+}
